a72f9e0dca4efe5af6e6b0c2937f24ae9bab551e
ekaats <egit@tuta.io>
2019-03-03 21:22:13 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ for VAR in `env`; do
     if [[ $VAR =~ ^MINIDLNA_ ]]; then
         upnp_speaker_name=`echo "$VAR" | sed -r "s/UPNP_SPEAKER_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]'`
         upnp_speaker_value=`echo "$VAR" | sed -r "s/.*=(.*)/\1/g"`
-        echo "${minidlna_name}=${upnp_speaker_value}" >> /etc/upnp_speaker.conf
+        echo "${mupnp_speaker_name}=${upnp_speaker_value}" >> /etc/upnp_speaker.conf
     fi
 done
 
-[ -f /var/run/upnp_speaker/minidlna.pid ] && rm -f /var/run/upnp_speaker/upnp_speaker.pid
+[ -f /var/run/upnp_speaker/upnp_speaker.pid ] && rm -f /var/run/upnp_speaker/upnp_speaker.pid
 
 exec upnp_speaker -d $@
